84f696dce05cbe67d8dc36b4d2bfa81b6f612585
John Mark Hawley <256080+jmhnilbog@users.noreply.github.com>
2022-01-03 22:25:33 -0500

    setup

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,21 @@
+// Get the sass related to the module.
 import "./markdown.scss";
 
-Hooks.once('init', async function () {
+import EasyMDE from "easymde";
+import { Editor } from "tinymce";
 
+const log = (...rest) => console.log(`MKD_IT_MORE: `, { ...rest })
+
+Hooks.once('init', async function () {
+    log('init');
+    TextEditor.create = async (options: TextEditor.Options, content: string) => {
+        const editor = new EasyMDE({ ...options } as EasyMDE.Options);
+        editor.value(content);
+
+        return editor as unknown as Editor;
+    }
 });
 
 Hooks.once('ready', async function () {
-
+    log('ready')
 });
--- webpack.config.ts
+++ webpack.config.ts
@@ -6,6 +6,7 @@ import { CleanWebpackPlugin } from 'clean-webpack-plugin';
 import CopyPlugin from 'copy-webpack-plugin';
 import ESLintPlugin from 'eslint-webpack-plugin';
 import globImporter from 'sass-glob-importer';
+import { BannerPlugin } from 'webpack';
 
 import glob from 'glob';
 
@@ -110,8 +111,9 @@ const configuration = (env: any) => {
                     {
                         from: 'static',
                         noErrorOnMissing: true,
-                    },
+                    }
                 ],
+
             }),
             new SymlinkWebpackPlugin([
                 { origin: '../packs', symlink: 'packs', force: true },
@@ -122,6 +124,117 @@ const configuration = (env: any) => {
                 filename: 'styles/[name].css',
                 chunkFilename: 'styles/[id].css',
             }),
+            new BannerPlugin({
+                banner: `# License information
+
+    This work uses the following libraries with their licenses attached.
+    
+    # [EasyMDE](https://github.com/Ionaru/easy-markdown-editor)
+    
+    ## The MIT License (MIT)
+    
+    Copyright (c) 2015 Sparksuite, Inc.
+    Copyright (c) 2017 Jeroen Akkerman.
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+    
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
+    © 2020 GitHub, Inc.
+    
+    # [Markdown-It](https://github.com/markdown-it/markdown-it)  
+    
+    ## The MIT License (MIT)
+    
+    Copyright (c) 2014 Vitaly Puzrin, Alex Kocharin.
+
+    Permission is hereby granted, free of charge, to any person
+    obtaining a copy of this software and associated documentation
+    files (the "Software"), to deal in the Software without
+    restriction, including without limitation the rights to use,
+    copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following
+    conditions:
+
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+    OTHER DEALINGS IN THE SOFTWARE.
+    
+    # [Codemirror](https://codemirror.net/)
+    
+    ## The MIT License (MIT)
+    
+    MIT License
+    
+    Copyright (C) 2017 by Marijn Haverbeke <marijnh@gmail.com> and others
+    
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+    
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+    
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+    
+    # [markdown-it-container](https://github.com/markdown-it/markdown-it-container)
+    
+    ## The MIT License (MIT)
+    
+    Copyright (c) 2015 Vitaly Puzrin, Alex Kocharin.
+
+    Permission is hereby granted, free of charge, to any person
+    obtaining a copy of this software and associated documentation
+    files (the "Software"), to deal in the Software without
+    restriction, including without limitation the rights to use,
+    copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the
+    Software is furnished to do so, subject to the following
+    conditions:
+
+    The above copyright notice and this permission notice shall be
+    included in all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+    NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+    HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+    FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+    OTHER DEALINGS IN THE SOFTWARE.`
+            })
         ],
     };
 
